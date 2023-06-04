c03d79681974ed7dfbc791eb3d4937bebad3abc9
kaezar3600 <cesar.contreras.medina@gmail.com>
2023-06-04 13:56:44 -0500

    configurando proyecto

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+
+const root = createRoot(document.getElementById('app'))
+root.render(<h1>Mikaela, te amo hijita!!! a tu amita Delia tambien.. las extranyo mucho</h1>)
